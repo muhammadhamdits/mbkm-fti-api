9da2ea13ebad7498dc59210238b7b1e35465137a
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-22 21:19:41 +0700

    fix auth error

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   if(req.path == '/api/login') return next()
   
-  const token = req.headers['authorization'].split(' ')[1]
+  authHeader = req.headers.authorization
+
+  if (!authHeader) return res.status(401).json({ message: 'No token provided' })
+
+  const token = authHeader.split(' ')[1]
   if(!token) return res.status(401).json({ message: 'No token provided' })
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
