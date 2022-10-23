ed497d084cefa624525066556e53edad198234df
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-23 11:59:07 +0700

    adjust update student program status to match web needs

--- validation/studentPrograms/updateStudentProgramStatusValidation.js
+++ validation/studentPrograms/updateStudentProgramStatusValidation.js
@@ -3,5 +3,6 @@ const { body } = require('express-validator')
 module.exports = [
   body('studentId').isInt().notEmpty(),
   body('programId').isInt().notEmpty(),
-  body('status').isString().notEmpty()
+  body('status').isString().notEmpty(),
+  body('reason').isString()
 ]
