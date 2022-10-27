0dfa3d15294ccf6fab65b1d202b4f5b81ce6df18
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-27 21:59:11 +0700

    add input field

--- validation/logbooks/updateLogbooksValidation.js
+++ validation/logbooks/updateLogbooksValidation.js
@@ -3,6 +3,7 @@ const { param, body } = require('express-validator')
 module.exports = [
   param('id').isInt().notEmpty(),
   body('status').isString().optional(),
+  body('reason').isString().optional(),
   body('courseId').isInt().optional(),
   body('achievementCode').isString().optional()
 ]
