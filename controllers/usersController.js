9053884e7499fc59a8ab0a3154c1b137a50e4d96
Muhammad Hamdi <muhammadhamdi702@gmail.com>
2022-11-06 07:50:26 +0700

    Enhance users controller (#26)
    * fix output
    
    * fix input
    
    * add users index endpoint
    
    * add logic

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,30 @@
 const Lecturer = require('../models').Lecturer
+const Admin = require('../models').Admin
+const Student = require('../models').Student
+
+const index = async (req, res) => {
+  lecturers = await Lecturer.findAll()
+  admins = await Admin.findAll()
+  students = await Student.findAll()
+
+  users = lecturers.map(lecturer => {
+    lecturer = lecturer.toJSON()
+    lecturer.username = lecturer.nip
+    lecturer.role = 'lecturer'
+    return lecturer
+  }).concat(admins.map(admin => {
+    admin = admin.toJSON()
+    admin.role = 'admin'
+    return admin
+  })).concat(students.map(student => {
+    student = student.toJSON()
+    student.username = student.nim
+    student.role = 'student'
+    return student
+  }))
+
+  return res.status(200).json({ users })
+}
 
 const getLecturers = async (req, res) => {
   lecturers = await Lecturer.findAll()
@@ -6,5 +32,6 @@ const getLecturers = async (req, res) => {
 }
 
 module.exports = {
+  index,
   getLecturers
 }
--- validation/studentPrograms/showStudentProgramValidation.js
+++ validation/studentPrograms/showStudentProgramValidation.js
@@ -2,5 +2,5 @@ const { param, query } = require('express-validator')
 
 module.exports = [
   param('programId').isInt().notEmpty(),
-  query('studentId').isInt()
+  query('studentId').isInt().optional()
 ]
